24c399109711716878ca2388c0590c4db5c86f5a
IanChen <wernercancel018@gmail.com>
2024-07-21 17:34:55 +0800

    add task

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-var p = document.getElementById('paragraph');
-var btn = document.getElementById('btn');
-var btn2 = document.getElementById('btn2');
-btn.onclick = function() {
-    p.innerHTML = "I change!";
-}
+var a_btn = document.getElementById('add_task')
+var r_btn = document.getElementById('remove_task')
+var task_list = document.getElementById('task-list')
 
-btn2.onclick = function() {
-    p.innerHTML = "this shit is here again"
+a_btn.onclick = function() {
+    var task_name = prompt("Enter new task name :")
+    if (task_name !== null && task_name !== "") {
+      var new_task = document.createElement('li')
+      var task_input = document.createElement('input')
+      var task_label = document.createElement('label')
+      task_input.type = 'checkbox'
+      task_label.innerHTML = task_name
+      new_task.appendChild(task_input)
+      new_task.appendChild(task_label)
+      task_list.appendChild(new_task)
+    }
 }
